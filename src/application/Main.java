6bd1bc473f33319292d13c803b8c0600ae125a6e
chaninkou <michaelkoulove@yahoo.com>
2018-11-06 23:24:51 -0800

    Made changes to the importing file

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			// Retrieve the information for pet food
-			InformationRetrieve.petFoodimport(InformationRetrieve.petFoodFile);
+			InformationRetrieve.petFoodimport(InformationRetrieve.petFoodFilePath);
 			
 			// Calling the home page
 			HomeStartPage homePage = new HomeStartPage();
--- src/backend/InformationRetrieve.java
+++ src/backend/InformationRetrieve.java
@@ -1,71 +1,47 @@
 package backend;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class InformationRetrieve {
 	
 //	public static final String petFoodFile = "dataBase/FoodInformation/petFoodData.csv";
-	public static final String petFoodFile = "src/dataBase/FoodInformation/petFoodData.csv";
+	// This is the file path
+	public static final String petFoodFilePath = "src/dataBase/FoodInformation/petFoodData.csv";
 	
-//	public static void petFoodimport(String outFile){
-//		String line;
-//		String[] fields;
-//		String imagePath;
-//		String name;
-//		String petKind;
-//		String Ingredient;
-//		int dailyFeeding;
-//		
-//		try {
-//			Scanner freader = new Scanner(outFile);
-//			while (freader.hasNextLine()) {
-//				line = freader.nextLine();
-//				fields = line.split(",");
-//				imagePath = fields[0];
-//				name = fields[1];
-//				petKind = fields[2];
-//				Ingredient = fields[3];
-//				dailyFeeding = Integer.parseInt(fields[4]);
-//				SelectedPetFood.petFoodList.add(new PetFood(imagePath,name,petKind,Ingredient,dailyFeeding));
-//			}
-//			freader.close(); // Close to unlock.
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			System.out.println("Retrieve not working");
-//		}	
-//	}
-	
-	public static void petFoodimport(String outFile){
-		String line = null;
+	// Importing from the csv file
+	public static void petFoodimport(String file){
+		String line;
 		String[] fields;
 		String imagePath;
 		String name;
 		String petKind;
 		String Ingredient;
 		int dailyFeeding;
+		File outFile = new File(file);
 		
 		try {
-			FileReader fileReader = new FileReader(outFile);
-			BufferedReader buffer = new BufferedReader(fileReader);
-			while ((line = buffer.readLine()) != null) {
+			Scanner freader = new Scanner(outFile);
+			while (freader.hasNextLine()) {
+				line = freader.nextLine();
 				fields = line.split(",");
 				imagePath = fields[0];
-				System.out.println(imagePath);
+//				System.out.println(imagePath);
 				name = fields[1];
-				System.out.println(name);
+//				System.out.println(name);
 				petKind = fields[2];
+//				System.out.println(petKind);
 				Ingredient = fields[3];
+//				System.out.println(Ingredient);
 				dailyFeeding = Integer.parseInt(fields[4]);
+//				System.out.println(dailyFeeding);
 				SelectedPetFood.petFoodList.add(new PetFood(imagePath,name,petKind,Ingredient,dailyFeeding));
 			}
-			buffer.close(); // Close to unlock.
-		} catch (Exception e) {
+			freader.close(); // Close to unlock.
+		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("Retrieve not working");
 		}	
 	}
-	
 }
