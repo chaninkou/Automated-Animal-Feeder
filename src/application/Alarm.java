a1c1cc8bc61a1aa66a53d4625472ffbcb888f5dc
Urbanham <Urbanham15@gmail.com>
2018-11-30 17:27:18 -0800

    Working on the alarm

--- src/application/Alarm.java
+++ src/application/Alarm.java
@@ -1,22 +1,68 @@
 package application;
 
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.Map;
+
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
-public class Alarm extends Application {
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        Scene scene = new Scene(root);
-
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.setTitle("Alarm");
-
-        stage.show();
-    }
+public class Alarm {
+	Map<Integer,String> numberMap;
+	Integer currSeconds; 
+	Thread thrd;
+	Integer hmsToSeconds(Integer h , Integer m , Integer s) {
+		Integer hToSeconds = h*3600;
+		Integer mToSeconds = m*60;
+		Integer total = hToSeconds + mToSeconds + s;
+		return total;
+	}
+	
+	LinkedList<Integer> secondsToHms(Integer currSecond){
+		Integer hours = currSecond /3600;
+		currSecond = currSecond%3600;
+		Integer minutes = currSecond / 60;
+		currSecond = currSecond%60;
+		Integer seconds = currSecond;
+		LinkedList<Integer> answer = new LinkedList<>();
+		answer.add(hours);
+		answer.add(minutes);
+		answer.add(seconds);
+		return answer;
+	}
+	void startCountdown() {
+		thrd = new Thread(new Runnable(){
+			
+		@Override
+		public void run() {
+			try {
+				while(true) {
+					//countdown here 
+					setOutput();
+					Thread.sleep(1000);
+					if(currSeconds ==0) {
+						thrd.stop();
+					}
+				}
+				
+			}
+			catch(Exception e){
+				
+			}
+		}
+	});
+	
+	}
+	void setOutput() {
+		LinkedList<Integer> currHms= secondsToHms(currSeconds);
+		System.out.println(currHms.get(0) + "." + currHms.get(1)+ "." + currHms.get(2));
+	}
 }
