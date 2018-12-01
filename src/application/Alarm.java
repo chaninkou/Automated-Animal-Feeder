4bde3d7923066e04cc0ad8916a38ce56dda28cf6
Urbanham <Urbanham15@gmail.com>
2018-11-30 23:34:59 -0800

    Added an easier way using minutes. Tomorrow will see how to implement into Our AAF

--- src/application/Alarm.java
+++ src/application/Alarm.java
@@ -59,6 +59,27 @@ public class Alarm {
 			}
 		}
 	});
+		
+//	//public class Alarm {
+//	    Scanner scan = new Scanner(System.in);
+//	    System.out.println("How much time (In Minutes) ?");
+//	    int timet= scan.nextInt() * 60; // Convert to seconds
+//	    long delay = timet * 1000;
+//
+//	    do
+//	    {
+//	      int minutes = timet / 60;
+//	      int seconds = timet % 60;
+//	      System.out.println(minutes +" minute(s), " + seconds + " second(s)");
+//	      Thread.sleep(1000);
+//	      timet = timet - 1;
+//	      delay = delay - 1000;
+//
+//	    }
+//	    while (delay != 0);
+//	    System.out.println("Time's Up!");
+//	  }
+//	}
 	
 	}
 	void setOutput() {
