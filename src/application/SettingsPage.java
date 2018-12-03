ddc721a53ab545d7591df735d5fef67723f816da
Urbanham <Urbanham15@gmail.com>
2018-12-03 10:31:47 -0800

    Quit button has been added. Also Countdown in settings now shows pop up alert when completed. Still need to implement adding to the actual log.

--- src/application/SettingsPage.java
+++ src/application/SettingsPage.java
@@ -2,13 +2,19 @@ package application;
 
 import java.util.List;
 
-import application.Alarm;
+import javax.swing.JOptionPane;
 
+import application.Alarm;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -17,6 +23,7 @@ public class SettingsPage extends BorderPane {
 	private Button toMenuPage;
 	private Button timer;
 	private Button dispenseDuration;
+	private int counter;
 	
 	public SettingsPage(){
 		Image image = new Image("backgroundPicture/alarmclock.jpg");
@@ -62,9 +69,47 @@ public class SettingsPage extends BorderPane {
 		// Making a refill button
 		timer = new Button("Count Down.");
 		timer.setId("timer");
+		timer.getStyleClass().add("settingPageButtons");
+		
+		timer.addEventHandler(MouseEvent.MOUSE_CLICKED,
+				new EventHandler<Event>() {
+			Boolean clicked = true;
+
+			@Override
+			public void handle(Event event) {
+				if (clicked == true) {	
+
+					int seconds =Integer.parseInt(JOptionPane
+							.showInputDialog("Please enter the seconds"));
+					
+					for(int i = seconds; i>=0; i--) {
+						try {
+							if(i >0) {
+								Thread.sleep(1000);
+								System.out.println(i);
+							}
+							else {
+								Alert alert = new Alert(AlertType.INFORMATION);
+								alert.setTitle("Information Dialog");
+								alert.setHeaderText(null);
+								alert.setContentText("AAF Will now Dispense");
+								
+								alert.showAndWait();
+							}
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}	
+					}
+				} 
+			}
+		});
+		
+		
 		
 		dispenseDuration = new Button("Set time for the duration of the AAF dispensement.");
 		dispenseDuration.setId("dispenseDuration");
+		dispenseDuration.getStyleClass().add("settingPageButtons");
 		
 		hbox.setPadding(new Insets(0,0,30,0));
 		hbox.setSpacing(50);
@@ -78,6 +123,11 @@ public class SettingsPage extends BorderPane {
 		// Need this to set it to the bottom of the GUI
 		setBottom(hbox);
 	}
+	
+	
+	
+	
+	
 	
 	
 	// Returning the value of toMenuPage
