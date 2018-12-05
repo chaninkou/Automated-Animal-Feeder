151d901e70600717d9d3f592a45c9fa966bcdfe6
Urbanham <Urbanham15@gmail.com>
2018-12-04 18:51:51 -0800

    fixed The problem with settings page

--- src/application/SettingsPage.java
+++ src/application/SettingsPage.java
@@ -2,11 +2,19 @@ package application;
 
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
+import application.Alarm;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -15,13 +23,14 @@ public class SettingsPage extends BorderPane {
 	private Button toMenuPage;
 	private Button timer;
 	private Button dispenseDuration;
+	private int counter;
 	
 	public SettingsPage(){
 		Image image = new Image("backgroundPicture/alarmclock.jpg");
 		ImageView imageView = new ImageView(image);
 		
-		imageView.setFitHeight(700);
-		imageView.setFitWidth(1200);
+		imageView.setFitHeight(1000);
+		imageView.setFitWidth(1800);
 		imageView.setPreserveRatio(false);
 		imageView.setSmooth(true);
 		imageView.setCache(true);
@@ -40,7 +49,8 @@ public class SettingsPage extends BorderPane {
 		HBox hbox = new HBox();
 		toMenuPage = new Button("Back");
 		toMenuPage.setId("back");
-	
+		toMenuPage.getStyleClass().add("backButtons");
+		
 		
 		hbox.setPadding(new Insets(30, 0, 0, 50));
 		hbox.setSpacing(100);
@@ -59,9 +69,47 @@ public class SettingsPage extends BorderPane {
 		// Making a refill button
 		timer = new Button("Count Down.");
 		timer.setId("timer");
+		timer.getStyleClass().add("settingPageButtons");
+		
+		timer.addEventHandler(MouseEvent.MOUSE_CLICKED,
+				new EventHandler<Event>() {
+			Boolean clicked = true;
+
+			@Override
+			public void handle(Event event) {
+				if (clicked == true) {	
+
+					int seconds =Integer.parseInt(JOptionPane
+							.showInputDialog("Please enter the seconds"));
+					
+					for(int i = seconds; i>=0; i--) {
+						try {
+							if(i >0) {
+								Thread.sleep(1000);
+								System.out.println(i);
+							}
+							else {
+								Alert alert = new Alert(AlertType.INFORMATION);
+								alert.setTitle("Information Dialog");
+								alert.setHeaderText(null);
+								alert.setContentText("AAF Will now Dispense");
+								
+								alert.showAndWait();
+							}
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}	
+					}
+				} 
+			}
+		});
+		
+		
 		
 		dispenseDuration = new Button("Set time for the duration of the AAF dispensement.");
 		dispenseDuration.setId("dispenseDuration");
+		dispenseDuration.getStyleClass().add("settingPageButtons");
 		
 		hbox.setPadding(new Insets(0,0,30,0));
 		hbox.setSpacing(50);
@@ -75,6 +123,10 @@ public class SettingsPage extends BorderPane {
 		// Need this to set it to the bottom of the GUI
 		setBottom(hbox);
 	}
+	
+	
+	
+	
 	
 	
 	
